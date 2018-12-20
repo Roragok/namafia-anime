4d4a89a6585f7f416296fb631b42da2bff92e03b
Dan <iconicsc@gmail.com>
2018-12-19 18:28:21 -0600

    moved folders around to get app to work

---
+++ stream/precache-manifest.38dc86d2f079bb375af0651de9e55a44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88440d130d22829fc302",
+    "url": "/build/static/css/main.6672ce9d.chunk.css"
+  },
+  {
+    "revision": "88440d130d22829fc302",
+    "url": "/build/static/js/main.88440d13.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/build/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "3923d73d01a4271659cc",
+    "url": "/build/static/js/runtime~main.3923d73d.js"
+  },
+  {
+    "revision": "3c10c72db23a388f35c36be63444e090",
+    "url": "/build/index.html"
+  }
+];
