262d923327b19269a8efbfbca0df14b3703a0d26
Roragok <roragok@gmail.com>
2025-04-13 16:18:37 +0000

    change path to restreamer rmtp url

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -8,7 +8,7 @@ const Player = (props, ref) => {
     return (
         <div className="aspect-ratio">
             <Iframe
-                url="https://stream.namafia.com/player.html"
+                url="https://stream.namafia.com/ed45857f-9b70-4519-87ba-b3c8797d5aa9.html"
                 height="100%"
                 id="player-embed"
                 className="restreamer-player"
